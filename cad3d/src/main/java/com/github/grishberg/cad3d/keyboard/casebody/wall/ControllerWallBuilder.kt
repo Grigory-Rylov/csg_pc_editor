b5350defabbaac30929fffe32bbb556d7038c1f2
Grirgory Rylov <grishberg@gmail.com>
2025-09-03 22:31:31 +0300

    left wall

--- cad3d/src/main/java/com/github/grishberg/cad3d/keyboard/casebody/wall/ControllerWallBuilder.kt
+++ cad3d/src/main/java/com/github/grishberg/cad3d/keyboard/casebody/wall/ControllerWallBuilder.kt
@@ -7,6 +7,7 @@ import com.github.grishberg.cad3d.keyboard.casebody.DefaultBottomEdgePatcher
 import com.github.grishberg.cad3d.keyboard.casebody.WallBottomEdgePatcher
 import com.github.grishberg.cad3d.keyboard.cfg.WallsSettings
 import eu.printingin3d.javascad.basic.Radius
+import eu.printingin3d.javascad.coords.V3d
 import eu.printingin3d.javascad.models.Abstract3dModel
 import eu.printingin3d.javascad.models.Hull
 import eu.printingin3d.javascad.models.Sphere
@@ -26,30 +27,22 @@ class ControllerWallBuilder(
 
     fun createWall(): Abstract3dModel {
         val models = mutableListOf<Abstract3dModel>()
-        models.add(backLeftCorner())
 
         val left = keyPlace.place(
-            0,
-            0,
-            KeyPlaceholder.placeHolderBackLeft().move(-cfg.outerLeftOffset, 0.0, cfg.outerBorderZOffset)
+            0, 0, KeyPlaceholder.placeHolderBackLeft().move(-cfg.outerLeftOffset, 0.0, cfg.outerBorderZOffset)
         )
 
         val right = keyPlace.place(
-            2,
-            0,
-            KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+            2, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
         )
 
         val mid = keyPlace.place(
-            1, 0,
-            KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+            1, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
         )
 
         val midLeft = keyPlace.place(
-            0, 0,
-            KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+            0, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
         )
-
 
         val backControllerLeft = controllerHolderWall.getWallPoint(left.move)
         val backControllerRight = controllerHolderWall.getWallPoint(right.move)
@@ -57,69 +50,30 @@ class ControllerWallBuilder(
         val backControllerMidLeft = controllerHolderWall.getWallPoint(midLeft.move)
 
 
+        models.add(backLeftCorner())
+
         if (isSkeletonMode) {
-            //top edge
-            val sphere = Sphere(Radius.fromRadius(cfg.borderThickness))
-            models.add(
-                Hull(
-                    sphere.move(backControllerLeft), sphere.move(backControllerRight)
-                )
-            )
-
-            //bottom edge
-
-            models.add(
-                Hull(
-                    bottomBorderObject().move(backControllerRight.projectionZ(cfg.borderThickness)),
-                    bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness))
-                )
-            )
-
-            //left vertical
-            models.add(Hull(
-                sphere.move(backControllerLeft),
-                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness)),
-            ))
-
-            //left corner - bottom
-            models.add(Hull(
+            skeletonWalls(
+                models,
+                backControllerLeft,
+                backControllerRight,
                 left,
-                bottomEdgePatcher.leftPoint(left),
-            ))
-
-            //left corner - back top controller
-            models.add(Hull(
-                left,
-                sphere.move(backControllerLeft),
-            ))
-
-            //mid - back top controller
-            models.add(Hull(
                 mid,
-                sphere.move(backControllerMid),
-            ))
-
-            //mid left - back top controller
-            models.add(Hull(
+                backControllerMid,
                 midLeft,
-                sphere.move(backControllerMidLeft),
-            ))
-
-            models.add(Hull(
-                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness)),
-                bottomEdgePatcher.leftPoint(left),
-            ))
-
-            // center left
-            val centerLeft = keyPlace.place(
-                2, 0,
-                KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+                backControllerMidLeft
             )
-
-            models.add(Hull(
-                centerLeft,
-                bottomEdgePatcher.backPoint(centerLeft),
-            ))
+        } else {
+            solidWalls(
+                models,
+                backControllerLeft,
+                backControllerRight,
+                left,
+                mid,
+                backControllerMid,
+                midLeft,
+                backControllerMidLeft
+            )
         }
 
         //models.add(backWall(0))
@@ -128,16 +82,186 @@ class ControllerWallBuilder(
         return Union(models)
     }
 
+    private fun skeletonWalls(
+        models: MutableList<Abstract3dModel>,
+        backControllerLeft: V3d,
+        backControllerRight: V3d,
+        left: Abstract3dModel,
+        mid: Abstract3dModel?,
+        backControllerMid: V3d,
+        midLeft: Abstract3dModel?,
+        backControllerMidLeft: V3d
+    ) {
+        //top edge
+        val sphere = Sphere(Radius.fromRadius(cfg.borderThickness))
+        models.add(
+            Hull(
+                sphere.move(backControllerLeft), sphere.move(backControllerRight)
+            )
+        )
+
+        //bottom edge
+
+        models.add(
+            Hull(
+                bottomBorderObject().move(backControllerRight.projectionZ(cfg.borderThickness)),
+                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness))
+            )
+        )
+
+        //left vertical
+        models.add(
+            Hull(
+                sphere.move(backControllerLeft),
+                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness)),
+            )
+        )
+
+        //left corner - bottom
+        models.add(
+            Hull(
+                left,
+                bottomEdgePatcher.leftPoint(left),
+            )
+        )
+
+        //left corner - back top controller
+        models.add(
+            Hull(
+                left,
+                sphere.move(backControllerLeft),
+            )
+        )
+
+        //mid - back top controller
+        models.add(
+            Hull(
+                mid,
+                sphere.move(backControllerMid),
+            )
+        )
+
+        //mid left - back top controller
+        models.add(
+            Hull(
+                midLeft,
+                sphere.move(backControllerMidLeft),
+            )
+        )
+
+        models.add(
+            Hull(
+                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness)),
+                bottomEdgePatcher.leftPoint(left),
+            )
+        )
+
+        // center left
+        val centerLeft = keyPlace.place(
+            2, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+        )
+
+        models.add(
+            Hull(
+                centerLeft,
+                bottomEdgePatcher.backPoint(centerLeft),
+            )
+        )
+    }
+
+    private fun solidWalls(
+        models: MutableList<Abstract3dModel>,
+        backControllerLeft: V3d,
+        backControllerRight: V3d,
+        left: Abstract3dModel,
+        mid: Abstract3dModel?,
+        backControllerMid: V3d,
+        midLeft: Abstract3dModel?,
+        backControllerMidLeft: V3d
+    ) {
+        //top edge
+        val sphere = Sphere(Radius.fromRadius(cfg.borderThickness))
+        models.add(
+            Hull(
+                sphere.move(backControllerLeft), sphere.move(backControllerRight)
+            )
+        )
+
+        //bottom edge
+
+        models.add(
+            Hull(
+                bottomBorderObject().move(backControllerRight.projectionZ(cfg.borderThickness)),
+                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness))
+            )
+        )
+
+        //left vertical
+        models.add(
+            Hull(
+                sphere.move(backControllerLeft),
+                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness)),
+            )
+        )
+
+        //left corner - bottom
+        models.add(
+            Hull(
+                left,
+                bottomEdgePatcher.leftPoint(left),
+            )
+        )
+
+        //left corner - back top controller
+        models.add(
+            Hull(
+                left,
+                sphere.move(backControllerLeft),
+            )
+        )
+
+        //mid - back top controller
+        models.add(
+            Hull(
+                mid,
+                sphere.move(backControllerMid),
+            )
+        )
+
+        //mid left - back top controller
+        models.add(
+            Hull(
+                midLeft,
+                sphere.move(backControllerMidLeft),
+            )
+        )
+
+        models.add(
+            Hull(
+                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness)),
+                bottomEdgePatcher.leftPoint(left),
+            )
+        )
+
+        // center left
+        val centerLeft = keyPlace.place(
+            2, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+        )
+
+        models.add(
+            Hull(
+                centerLeft,
+                bottomEdgePatcher.backPoint(centerLeft),
+            )
+        )
+    }
+
     private fun backWall(column: Int): Abstract3dModel {
         val left = keyPlace.place(
-            column,
-            0,
-            KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+            column, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
         )
         val right = keyPlace.place(
-            column,
-            0,
-            KeyPlaceholder.placeHolderBackRight().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+            column, 0, KeyPlaceholder.placeHolderBackRight().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
         )
 
         val backControllerRight = controllerHolderWall.getWallPoint(right.move)
@@ -204,10 +328,12 @@ class ControllerWallBuilder(
 
     private fun backLeftCorner(): Abstract3dModel {
 
-        val back =
-            keyPlace.place(0, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset))
-        val left =
-            keyPlace.place(0, 0, KeyPlaceholder.placeHolderBackLeft().move(-cfg.outerLeftOffset, 0.0, cfg.outerBorderZOffset))
+        val back = keyPlace.place(
+            0, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.outerVerticalOffset, cfg.outerBorderZOffset)
+        )
+        val left = keyPlace.place(
+            0, 0, KeyPlaceholder.placeHolderBackLeft().move(-cfg.outerLeftOffset, 0.0, cfg.outerBorderZOffset)
+        )
 
         val backControllerBack = controllerHolderWall.getWallPoint(back.move)
         val backControllerLeft = controllerHolderWall.getWallPoint(left.move)
@@ -215,16 +341,12 @@ class ControllerWallBuilder(
         val border = Utils.hull(
             verticalCube(
                 keyPlace.place(
-                    0,
-                    0,
-                    KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.verticalOffset, cfg.borderZOffset)
+                    0, 0, KeyPlaceholder.placeHolderBackLeft().move(0.0, cfg.verticalOffset, cfg.borderZOffset)
                 )
             ),
             verticalCube(
                 keyPlace.place(
-                    0,
-                    0,
-                    KeyPlaceholder.placeHolderBackLeft().move(-cfg.leftOffset, 0.0, cfg.borderZOffset)
+                    0, 0, KeyPlaceholder.placeHolderBackLeft().move(-cfg.leftOffset, 0.0, cfg.borderZOffset)
                 )
             ),
             back,
@@ -235,8 +357,7 @@ class ControllerWallBuilder(
             val objects = mutableListOf<Abstract3dModel>()
             objects.add(border)
 
-            val sphere = Sphere(Radius.fromRadius(cfg.borderThickness))
-            /*
+            val sphere = Sphere(Radius.fromRadius(cfg.borderThickness))/*
             objects.add(Utils.hull(left, sphere.move(backControllerLeft)))
             objects.add(Utils.hull(back, sphere.move(backControllerBack)))
             objects.add(
@@ -279,12 +400,34 @@ class ControllerWallBuilder(
             return Union(objects)
         }
 
-        val wall = Utils.hull(
-            left, back,
-            bottomEdgePatcher.leftPoint(left),
-            bottomEdgePatcher.backPoint(back),
+        val sphere = Sphere(Radius.fromRadius(cfg.borderThickness))
+
+
+        return Union(
+            border, Utils.hull(
+                left, back,
+                sphere.move(backControllerLeft),
+
+                ), Utils.hull(
+                back,
+                sphere.move(backControllerBack),
+                sphere.move(backControllerLeft),
+            ), Utils.hull(
+                sphere.move(backControllerBack),
+                sphere.move(backControllerLeft),
+                bottomBorderObject().move(backControllerBack.projectionZ(cfg.borderThickness)),
+                bottomBorderObject().move(backControllerLeft.projectionZ(cfg.borderThickness)),
+            ), Utils.hull(
+                left,
+                sphere.move(backControllerLeft),
+                bottomBorderObject().move(left.move.projectionZ(cfg.borderThickness)),
+            ), Utils.hull(
+                sphere.move(backControllerLeft),
+                sphere.move(backControllerLeft.projectionZ(cfg.borderThickness)),
+                bottomBorderObject().move(left.move.projectionZ(cfg.borderThickness)),
+            )
+
         )
-        return Union(border, wall)
     }
 
     private fun verticalCube(obj: Abstract3dModel): Abstract3dModel {
