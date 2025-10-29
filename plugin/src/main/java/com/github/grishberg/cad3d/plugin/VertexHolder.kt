7753317b1026d220c62b436ad5b780c3f9ee36ab
Grirgory Rylov <grishberg@gmail.com>
2025-10-29 07:26:06 +0300

    VertexHolder.kt

--- plugin/src/main/java/com/github/grishberg/cad3d/plugin/VertexHolder.kt
+++ plugin/src/main/java/com/github/grishberg/cad3d/plugin/VertexHolder.kt
@@ -1,28 +1,3 @@
-package com.github.grishberg.cad3d.plugin;
+package com.github.grishberg.cad3d.plugin
 
-public class VertexHolder {
-
-    private final float[] vertex;
-    private final float[] normals;
-    private final int verticesCount;
-
-    public VertexHolder(float[] vertex, float[] normals, int verticesCount) {
-        this.vertex = vertex;
-        this.normals = normals;
-        this.verticesCount = verticesCount;
-    }
-
-
-    public float[] getVertex() {
-        return vertex;
-    }
-
-    public float[] getNormals() {
-        return normals;
-    }
-
-    public int getVerticesCount() {
-        return verticesCount;
-    }
-
-}
+class VertexHolder(val vertex: FloatArray, val normals: FloatArray, val verticesCount: Int)
